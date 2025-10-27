d4bfd5f388298e1731ba842234ea1b3a1ce9ed28
Hardik_Raj <137692259+Eraxty@users.noreply.github.com>
2025-10-28 00:05:13 +0530

    Q4-Q5

---
+++ Python Fundamentals/Q4.py
@@ -0,0 +1,12 @@
+#Modify the above program to print `5@10@9` as a single output
+
+#previous program
+x = 0 
+x += 5 
+x *= 2
+x -= 1
+
+
+#modified program
+x = "5@10@9"
+print(x)
---
+++ Python Fundamentals/Q5.py
@@ -0,0 +1,6 @@
+#Write a program (maximum 3 lines) that assigns the first
+#  5 multiples of a number to 5 variables and prints them.
+
+num = int(input("Enter a number: "))
+a,b,c,d,e = num, num*2, num*3, num*4, num*5
+print(a, b, c, d, e)
