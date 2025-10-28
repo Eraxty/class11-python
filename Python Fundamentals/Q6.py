e38f723f6da36a83c1fa84c1818a8168c9281709
Hardik_Raj <137692259+Eraxty@users.noreply.github.com>
2025-10-28 22:52:19 +0530

    Update Q6.py

---
+++ Python Fundamentals/Q6.py
@@ -0,0 +1,8 @@
+# Accept marks in 5 subjects from the user and output the average marks.
+a = int(input("Enter marks for subject 1: "))
+b = int(input("Enter marks for subject 2: "))   
+c = int(input("Enter marks for subject 3: "))
+d = int(input("Enter marks for subject 4: "))
+e = int(input("Enter marks for subject 5: "))
+average = (a + b + c + d + e) / 5
+print("The average marks are:", average)
